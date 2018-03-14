6603a01c67d28f3bfa7fb69793a9839b06f7a63d
chetans9 <chetanshenai9@gmail.com>
2018-03-14 16:09:09 +0530

    permissions

--- resources/views/User/registration.blade.php
+++ resources/views/User/registration.blade.php
@@ -4,5 +4,9 @@
 </head>
 <body>
 <h1>Registration Form</h1>
+{{ Form::open(array('url'=>'/user/registration')) }}
+{{ Form:: text('fname')}}
+{{ Form:: text('lname')}}
+{{ Form::submit('submit') }}
 </body>
 </html>
